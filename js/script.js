08cd7adb0d18420d3ebb0a68fa2c42f0b41fada3
Andres Ortiz <andresortizbe@gmail.com>
2020-09-20 18:33:39 -0500

    subiendo archivos
    archivos

---
+++ js/script.js
@@ -0,0 +1,119 @@
+function click_home()
+  {
+    document.getElementById('box-home').style.display = 'initial';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  }
+  function click_about()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'initial';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  }
+  function click_cur()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'initial';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  }
+  function click_portfolio()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'initial';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  }
+  function click_blog()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'initial';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  }
+  function click_contact()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'initial';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  
+  }  
+  function click_face()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'initial';
+  
+  }
+  function click_twitter()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'initial';
+    document.getElementById('box-instagram').style.display = 'none';
+    document.getElementById('box-face').style.display = 'none';
+  
+  }
+  function click_insta()
+  {
+    document.getElementById('box-home').style.display = 'none';
+    document.getElementById('box-about').style.display = 'none';
+    document.getElementById('box-curriculum').style.display = 'none';
+    document.getElementById('box-portfolio').style.display = 'none';
+    document.getElementById('box-blog').style.display = 'none';
+    document.getElementById('box-contact').style.display = 'none';
+    document.getElementById('box-twitter').style.display = 'none';
+    document.getElementById('box-instagram').style.display = 'initial';
+    document.getElementById('box-face').style.display = 'none';
+  
+  }
+
+
+
+
+  
+  
+  
